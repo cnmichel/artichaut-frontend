fabf8818844b69972c3ca946b4f82bcf2fbf8193
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-02-09 01:37:25 +0100

    feat: added a page layout for admin panel

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,43 @@
 <script setup lang="ts">
-
+import { computed } from "vue";
+import { useRouter } from 'vue-router';
 import Header from "@/components/HeaderComponent.vue";
-import Footer from "@/components/FooterComponent.vue";
+import SideMenu from "@/components/SideMenuComponent.vue";
+
+const router = useRouter()
+const page = computed(() => router.currentRoute.value.name)
 </script>
 
-<template>
-  <header>
-    <Header />
-  </header>
-  <main>
-    <router-view></router-view>
-  </main>
-  <footer>
-    <Footer />
-  </footer>
+<template class="light">
+    <el-container class="container-layout">
+
+      <el-aside width="200px">
+        <SideMenu/>
+      </el-aside>
+
+      <el-container>
+        <el-header>
+          <Header :title="page"/>
+        </el-header>
+        <el-main>
+          <el-scrollbar>
+            <router-view/>
+          </el-scrollbar>
+        </el-main>
+      </el-container>
+
+    </el-container>
 </template>
 
 <style scoped>
-
+.container-layout .el-header {
+  width: 100%;
+  position: relative;
+}
+.container-layout .el-main {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  background-color: #d8d8d8;
+}
 </style>
