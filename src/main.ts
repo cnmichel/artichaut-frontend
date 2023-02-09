d1a86ac7a981f285b63cbd1a8fda25b6dcae3533
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-02-09 11:43:17 +0100

    chore: updated gitignore for env file and added axios default api base url

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
+import axios from "axios/index";
 
 // @ts-ignore
 import router from './router'
 
 import './assets/main.css'
 import 'element-plus/dist/index.css'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 
 createApp(App)
     .use(router)
