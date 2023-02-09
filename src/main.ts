ef35766d4951480b80f3811d0b5ab62af7d2bab8
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-02-09 16:13:02 +0100

    feat: added api methods for resources

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
-import axios from "axios";
 
 // @ts-ignore
 import router from './router'
 
 import './assets/main.css'
 import 'element-plus/dist/index.css'
-
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 
 createApp(App)
     .use(router)
