360651270c5f0ede9744fc2bade8a66afd96f643
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-02-09 01:32:19 +0100

    chore: added elementplus library for vue.js

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+
 // @ts-ignore
 import router from './router'
 
 import './assets/main.css'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(ElementPlus)
+    .mount('#app')
