06b2adb7b0d4a03c1ff98d431846736b121e1ba1
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-02-09 01:35:03 +0100

    feat: added side menu component for admin panel navigation

---
+++ src/components/SideMenuComponent.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { shallowRef } from 'vue';
+import { useRouter } from 'vue-router';
+import {
+  Document,
+  Menu as IconMenu,
+  Goods,
+  Discount,
+  VideoCamera,
+  PictureFilled
+} from '@element-plus/icons-vue';
+
+const router = useRouter()
+const pages = shallowRef([
+  { name: "Actualités", icon: Document },
+  { name: "Avantages", icon: IconMenu },
+  { name: "Produits", icon: Goods },
+  { name: "Promotions", icon: Discount },
+  { name: "Vidéos", icon: VideoCamera },
+  { name: "Hero", icon: PictureFilled }
+]);
+
+const handleClick = ({ index }: any) => {
+  router.push({name: index});
+}
+</script>
+
+<template>
+  <el-menu active-text-color="#00B561" style="height: 100vh; position: fixed; width: inherit">
+    <el-menu-item v-for="({ name, icon }, index) in pages"
+                  :key="index"
+                  :index="name"
+                  @click="handleClick">
+      <el-icon><Component :is="icon"/></el-icon>
+      <span>{{name}}</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<style scoped>
+.el-menu-item span {
+  font-weight: bold;
+}
+</style>
