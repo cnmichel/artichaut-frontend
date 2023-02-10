d2a2f4ba2017b55cb1638436847f61d80dc151dd
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-02-10 10:08:58 +0100

    chore: removed axios base url from main ts file

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,9 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 // @ts-ignore
 import router from './router'
-import axios from 'axios';
 
 import './assets/main.css'
 import 'element-plus/dist/index.css'
-
-axios.defaults.baseURL = "http://localhost/api"
 
 createApp(App)
     .use(router)
