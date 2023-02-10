cbc4aa8af87fe46be4004433efdc146a5a73a044
walczak <adrien.walczak@le-campus-numerique.fr>
2023-02-10 08:56:26 +0100

    (feature): update AdminVideoVue

--- src/views/admin/AdminVideoView.vue
+++ src/views/admin/AdminVideoView.vue
@@ -1,3 +1,85 @@
+<script setup lang="ts">
+import { ref, reactive, computed, onMounted } from 'vue';
+import axios from 'axios';
+import { slice } from 'lodash';
+
+interface Videos {
+    title: string,
+    content: string,
+    url: string
+}
+
+const videos = reactive({ items: <Videos[]>[] })
+const totalVideo = computed(() => videos.items.length);
+const paginateVideos = computed(() => {
+    const start = (current.page -1) * pageSize.value;
+    const end = start + pageSize.value;
+    return slice(videos.items, start, end);
+});
+const pageSize = ref(5);
+let current = reactive({ page: 1 });
+
+onMounted(async() => {
+    await axios.get('/videos')
+        .then((response) => { 
+        videos.items = response.data;
+    });
+});
+
+const handleCurrentChange = (val: number) => {
+    if (val !== current.page) {
+        current.page = val;
+        getPagination(videos.items, current.page);
+    }
+};
+
+const getPagination = (videos: Videos[], page: number) => {
+    const start = (page -1) * pageSize.value;
+    const end = start + pageSize.value;       
+};
+
+</script>
+
 <template>
-  <h1>VIDEO</h1>
+<el-card class="box-card">
+    <div class="">
+        <div id="divprincipale" ref="videos" v-for="video in paginateVideos" class="shadow-lg rounder-lg p-8 grid grid-cols-3 grid-flow-col gap-4">
+            <div class="row-span-3" id="img">
+              <iframe width="250" height="150" v-bind:src="video.url" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+            <div class="col-span-2 underline">{{ video.title}}</div>
+            <div class="row-span-2 col-span-2">{{ video.content }}</div>
+        </div>
+   <el-pagination 
+   :page-size="pageSize" 
+   background layout="prev, pager, next" 
+   :total="totalVideo" 
+   v-model="current" 
+   @current-change="handleCurrentChange"
+   id="pagination"/>
+    </div>
+</el-card>
 </template>
+
+<style scoped>
+.box-card {
+  height: 100%;
+}
+
+img{
+  width: 250px;
+  height: 150px;
+}
+
+#pagination{
+  justify-content: center;
+  margin-top: 10px;
+}
+
+#divprincipale{
+   margin-left: 150px;
+   margin-right: 150px;
+
+}
+
+</style>
