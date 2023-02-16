ed922bacc0e138efd6484b9977792dcd4f3023d0
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-02-16 23:44:15 +0100

    feat: reworked header and side menu component

--- src/components/admin/HeaderComponent.vue
+++ src/components/admin/HeaderComponent.vue
@@ -1,22 +1,33 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { useRouter } from 'vue-router';
-import { ArrowLeft } from '@element-plus/icons-vue';
+import { ArrowLeft, HomeFilled } from '@element-plus/icons-vue';
 
 defineProps<{
   title?: any
 }>()
 
 const router = useRouter();
-
+const current = computed(() => router.currentRoute.value.path);
 const goBack = () => {
   router.push("/admin/home");
 }
 </script>
 
 <template>
-  <el-page-header :icon="ArrowLeft" @back="goBack" style="position: relative">
+  <el-page-header v-if="current !== '/admin/home'" :icon="ArrowLeft" @back="goBack" style="position: relative">
     <template #title>
       Retour
+    </template>
+    <template #content>
+      <span class="text-large font-bold mr-3">{{title}}</span>
+    </template>
+  </el-page-header>
+  <el-page-header v-else :icon="null" style="position: relative">
+    <template #title>
+      <el-icon :size="19" style="vertical-align: sub" >
+        <HomeFilled />
+      </el-icon>
     </template>
     <template #content>
       <span class="text-large font-bold mr-3">{{title}}</span>
