d942187ca479a9e8f28482895f01f63bb1f240da
Rodalljuhziz <adrien.walczak@le-campus-numerique.fr>
2023-02-16 13:48:14 +0100

    (feature): Create HomeView and feature/homepage branch

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+
+const i18n = createI18n({
+  // something vue-i18n options here ...
+})
+
+const app = createApp({
+  // something vue options here ...
+})
+
+app.use(i18n)
+app.mount('#app')
+
+</script>
