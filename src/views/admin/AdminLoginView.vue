2e4091a233c5ca03c7de3dbbb35fddd928f9b8b3
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-02-20 13:42:23 +0100

    feat: pimped admin login view

--- src/views/admin/AdminLoginView.vue
+++ src/views/admin/AdminLoginView.vue
@@ -1,10 +1,46 @@
 <template>
-  <h1>ADMIN</h1>
-  <form @submit.prevent="submit">
-    <input type="email" name="email" v-model="user.email" />
-    <input type="password" name="password" v-model="user.password" />
-    <button type="submit">Submit</button>
-  </form>
+  <div class="flex justify-center items-center h-full">
+    <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+      <form @submit.prevent="submit">
+        <p class="mb-4">Please login to your account</p>
+        <div class="mb-4">
+          <input
+              name="email" v-model="user.email"
+              type="email"
+              class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleFormControlInput1"
+              placeholder="Username"
+          />
+        </div>
+        <div class="mb-4">
+          <input
+              name="password" v-model="user.password"
+              type="password"
+              class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleFormControlInput1"
+              placeholder="Password"
+          />
+        </div>
+        <div class="text-center pt-1 pb-1">
+          <button
+              class="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
+              type="submit"
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="light"
+              style="
+                            background: linear-gradient(
+                              to right,
+                              #006838,
+                              #00B561
+                            );
+                          "
+          >
+            Log in
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script setup>
