658d28b63ab6f25be906ac8513210f97a47eddae
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-02-20 01:17:58 +0100

    chore: moved header component to homepage view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-import { computed } from "vue";
 import { useRouter } from 'vue-router';
-import HeaderComponent from "@/components/HeaderComponent.vue";
 
 const router = useRouter()
 </script>
 
 <template class="light">
-  <HeaderComponent/>
   <router-view/>
 </template>
