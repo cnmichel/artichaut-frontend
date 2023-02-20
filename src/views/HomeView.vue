d8b098b7b536938a3e6a073f47fc22300b6ec6b3
Rodalljuhziz <adrien.walczak@le-campus-numerique.fr>
2023-02-20 08:44:12 +0100

    (feature): Add FooterComponent.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
-<script lang="ts">
+<script setup lang="ts">
+
+import Footer from '@/components/FooterComponent.vue';
 
 </script>
 
 <template>
 
-  <h1>{{ $t("message.H1") }}</h1>
+  <h1>{{ $t('message.H1') }}</h1>
+
+  <Footer />
+
 </template>
