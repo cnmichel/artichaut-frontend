b09856c0c7dfd1aed0a9c088753f35160bb201e3
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-02-21 08:56:11 +0100

    fix: fixed import for tw elements

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
-import 'tw-elements';
 // @ts-ignore
 import router from './router'
 // @ts-ignore
