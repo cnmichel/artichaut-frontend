872afecf36f92c8d06ad4fd5b0e93e19ecf1de3d
AZYxV <nschpro@gmail.com>
2023-02-24 09:47:11 +0100

    (fix): fix nav

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-
 <script setup lang="ts">
 import Hero from "@/components/HeroComponent.vue";
-import Features from "@/components/FeaturesComponent.vue";
 import Header from "@/components/HeaderComponent.vue";
+
 import Footer from '@/components/FooterComponent.vue';
+
 </script>
 
 <template>
