d01c2b680ae97fec5df6c7a3b5e43569f801ab86
Rodalljuhziz <adrien.walczak@le-campus-numerique.fr>
2023-02-27 15:01:35 +0100

    (feature):Add ArticlesComponent

---
+++ src/components/NewsComponent.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ElCarousel, ElCarouselItem} from 'element-plus';
+import {getArticles} from '@/services/api.js';
+import {reactive, onMounted} from 'vue';
+
+interface Articles {
+  title: string,
+  content: string,
+  image: string
+}
+
+const articles = reactive({ items: <Articles[]>[] });
+
+onMounted(async() => {
+  getArticles()
+      .then((data:any) => {
+        articles.items = data;
+      });
+});
+
+</script>
+
+<template>
+  <el-carousel height="80vh" style=" border-radius: 15px; margin:0 24px" trigger="click" indicator-position="inside" >
+    <el-carousel-item v-for="article in articles.items" :key="article.title">
+      <div class="bg-no-repeat bg-cover h-full"
+           :style="{ backgroundImage: `url(${article.image})`}">
+        <div class="text-center text-white px-6 md:px-12 grid items-center h-full">
+            <div class="text-5xl font-bold mt-0 mb-6">{{ article.title}}</div>
+            <div class="text-3xl font-bold mb-8">{{ article.content }}</div>
+        </div>
+      </div>
+    </el-carousel-item>
+  </el-carousel>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import Hero from "@/components/HeroComponent.vue";
 import Header from "@/components/HeaderComponent.vue";
-
 import Footer from '@/components/FooterComponent.vue';
+import News from '@/components/NewsComponent.vue';
 
 </script>
 
@@ -16,6 +16,7 @@ import Footer from '@/components/FooterComponent.vue';
     <section class=" flex items-center justify-center h-80 mb-12 bg-fixed bg-center bg-cover background-parralax-one">
     </section>
     <Discover/>
+    <News/>
   </main>
   <footer>
     <Footer/>
