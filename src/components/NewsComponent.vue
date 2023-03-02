88113a39139500683d9a1f062dd5b2fe1cd286ae
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-03-02 15:08:20 +0100

    refactor: code review on news and video component

--- src/components/NewsComponent.vue
+++ src/components/NewsComponent.vue
@@ -1,47 +1,43 @@
 <script setup lang="ts">
-import { ElCarousel, ElCarouselItem} from 'element-plus';
-import {getArticles} from '@/services/api.js';
-import {reactive, onMounted} from 'vue';
+import { reactive, onBeforeMount } from 'vue';
+import { getArticles } from '@/services/api.js';
+import { ElCarousel, ElCarouselItem } from 'element-plus';
 
-interface Articles {
+interface Article {
   title: string,
   content: string,
   image: string
 }
 
-const articles = reactive({ items: <Articles[]>[] });
+const articles = reactive({ items: <Article[]>[] })
 
-onMounted(async() => {
-  getArticles()
-      .then((data:any) => {
-        articles.items = data;
-      });
-});
-
+onBeforeMount(() => {
+  getArticles().then((data:any) => {
+    articles.items = data;
+  });
+})
 </script>
 
 <template>
   <a id="news"></a>
-  <h1>{{ $t('fields.newsTitle') }}</h1>
-  <el-carousel :interval="7000" height="80vh" style=" border-radius: 15px; margin:0 24px" trigger="click" indicator-position="inside" >
-    <el-carousel-item v-for="article in articles.items" :key="article.title">
-      <div class="bg-no-repeat bg-cover h-full"
-           :style="{ backgroundImage: `url(${article.image})`}">
-        <div class="text-center text-white px-6 md:px-12 grid items-center h-full backdrop-opacity-20 backdrop-invert bg-black/30">
+  <div v-if="articles.items !== []">
+    <h1 class="text-5xl font-bold text-center mt-0 mb-6">{{ $t('titles.news') }}</h1>
+    <el-carousel :interval="7000" height="70vh" trigger="click">
+      <el-carousel-item v-for="article in articles.items" :key="article.title">
+        <div class="bg-no-repeat bg-cover h-full" :style="{ backgroundImage: `url(${article.image})`}">
+          <div class="text-center text-white px-6 md:px-12 grid items-center h-full backdrop-opacity-20 backdrop-invert bg-black/30">
             <div class="background text-5xl font-bold mt-0 mb-6">{{ article.title}}</div>
             <div class="background text-3xl font-bold mb-8">{{ article.content }}</div>
+          </div>
         </div>
-      </div>
-    </el-carousel-item>
-  </el-carousel>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <style scoped>
-
-h1{
-  font-size: xxx-large;
-  text-align: center;
-  font-weight: bolder;
+.el-carousel {
+  border-radius: 15px;
+  margin: 24px;
 }
-
 </style>
