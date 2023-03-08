4ef3307f38bb749b0dc94848b9cb9e3606e36a9c
cnmichel <michel.nguyen@le-campus-numerique.fr>
2023-03-08 14:12:31 +0100

    feat: added control panel on user account button

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 import { inject, onBeforeMount, ref, computed } from "vue";
 import { useI18n } from "vue-i18n";
-import { getActivePromo } from '@/services/api.js'
+import { revokeToken } from '@/services/auth.js';
+import { getActivePromo } from '@/services/api.js';
 import { Expand, Fold, UserFilled } from "@element-plus/icons-vue";
 import { isEmpty } from 'lodash';
 
 const { t } = useI18n();
+
+const isLogged = computed(() => !!localStorage.getItem('user_token'))
+const userName = computed(() => {
+  const email = localStorage.getItem('user_email');
+  const name = localStorage.getItem('user_name');
+  return name ? name : email
+})
 
 const promo = ref('')
 const showMenu = ref(false)
@@ -19,6 +27,7 @@ const pages = computed(() => [
   { path: "/ourhotel", name: t('menus.ourHotel'), anchor:"ourHotel"},
   { path: "/reviews", name: t('menus.reviews'), anchor:"reviews"}
 ]);
+
 const toggleMenu = () => {
   showMenu.value = !showMenu.value;
 }
@@ -26,6 +35,15 @@ const toggleMenu = () => {
 const handleLangSelect = (code: string) => {
   localStorage.setItem('language', code);
   location.reload();
+}
+
+const handleLogout = () => {
+  const token = localStorage.getItem('user_token');
+  revokeToken(token).then(() => {
+    localStorage.removeItem('user_email');
+    localStorage.removeItem('user_token');
+    location.reload();
+  })
 }
 
 onBeforeMount(() => {
@@ -36,16 +54,18 @@ onBeforeMount(() => {
 </script>
 
 <template>
+  <!-- Promo banner -->
   <a id="home">
     <div v-if="promo !== ''" class="container-fluid w-full flex justify-center bg-tonic">
       <p class="p-2 text-white text-center" >{{ promo }}</p>
     </div>
   </a>
-
-  <nav class="sticky top-0 z-20 bg-white/90 dark:bg-white-100/80 backdrop-blur navbar shadow-2xl shadow-gray-100/5 border-gray-100 dark:border-gray-800 peer-checked:navbar-active dark:shadow-none">
+  <!-- Navbar -->
+  <nav class="sticky top-0 z-20 bg-white/90 backdrop-blur navbar shadow-2xl shadow-gray-600/5 peer-checked:navbar-active dark:shadow-none">
     <div class="flex flex-wrap items-center justify-between py-2.5 px-6 md:px-12 lg:py-0 lg:px-6">
       <div class="w-full relative flex items-center justify-between gap-6 md:gap-0">
 
+        <!-- Navbar burger-->
         <div class="lg:hidden">
           <el-button v-if="!showMenu" size="large" color="#253343" plain link @click="toggleMenu">
             <template v-slot:icon>
@@ -62,21 +82,24 @@ onBeforeMount(() => {
             </template>
           </el-button>
         </div>
-
+        <!-- Main logo -->
         <a class="relative" href="#" aria-label="logo">
           <img src="@/assets/artichaut-logo.png" class="h-12" alt="Logo Artichaut Hotel" loading="lazy" />
         </a>
-
+        <!-- Navbar horizontal web -->
         <div class="nav-horizontal items-center xs:max-lg:hidden">
           <el-menu mode="horizontal" :ellipsis="false" text-color="#253343" active-text-color="#00B561">
-            <el-menu-item v-for="({ path, name, anchor }, index) in pages" :id="name" :key="index" :index=" path"
+            <el-menu-item v-for="({ path, name, anchor }, index) in pages" :id="name" :key="index" :index="path"
                           class="transition duration-300 ease-in-out">
-              <a :href="'#' + anchor">{{name}}</a>
+              <a :href="'#' + anchor">
+                <span>{{ name }}</span>
+              </a>
             </el-menu-item>
           </el-menu>
         </div>
-
+        <!-- Navbar extra -->
         <div class="navbar-end flex flex-wrap items-center">
+          <!-- Lang buttons -->
           <div class="xs:max-lg:hidden">
             <el-dropdown trigger="click" @command="handleLangSelect">
               <el-button size="large" style="padding: 0" circle text>
@@ -87,43 +110,72 @@ onBeforeMount(() => {
                   <el-dropdown-item v-for="({ code, label }, index) in availableLangs"
                                     :key="index"
                                     :index="code"
-                                    :command="code"
-                  >
+                                    :command="code">
                     {{ label }}
                   </el-dropdown-item>
                 </el-dropdown-menu>
               </template>
             </el-dropdown>
           </div>
-          <router-link to="/login" class="xs:max-lg:hidden">
-            <el-button size="large" round color="#253343" data-te-ripple-init data-te-ripple-color="light"
-                       class="ml-4 leading-normal transition duration-150 ease-in-out">
-              <template v-slot:icon>
-                <el-icon :size="28" class="no-inherit">
-                  <UserFilled style="width: 2em; height: 2em; margin-right: 4px"/>
-                </el-icon>
-              </template>
-              {{ $t('buttons.my-account') }}
-            </el-button>
-          </router-link>
-          <router-link to="/login" class="lg:hidden">
-            <el-button size="large" circle color="#253343" data-te-ripple-init data-te-ripple-color="light"
-                       class="ml-4 leading-normal transition duration-150 ease-in-out">
-              <template v-slot:icon>
-                <el-icon :size="24" class="no-inherit">
-                  <UserFilled style="width: 2em; height: 2em"/>
-                </el-icon>
-              </template>
-            </el-button>
-          </router-link>
+          <!-- Account buttons -->
+          <el-dropdown trigger="click">
+            <div>
+              <div class="xs:max-lg:hidden">
+                <el-button size="large" round color="#253343" data-te-ripple-init data-te-ripple-color="light"
+                           class="ml-4 leading-normal transition duration-150 ease-in-out">
+                  <template v-slot:icon>
+                    <el-icon :size="28" class="no-inherit">
+                      <UserFilled style="width: 2em; height: 2em; margin-right: 4px"/>
+                    </el-icon>
+                  </template>
+                  {{ $t('buttons.my-account') }}
+                </el-button>
+              </div>
+              <div class="lg:hidden">
+                <el-button size="large" circle color="#253343" data-te-ripple-init data-te-ripple-color="light"
+                           class="ml-4 leading-normal transition duration-150 ease-in-out">
+                  <template v-slot:icon>
+                    <el-icon :size="24" class="no-inherit">
+                      <UserFilled style="width: 2em; height: 2em"/>
+                    </el-icon>
+                  </template>
+                </el-button>
+              </div>
+            </div>
+            <!-- Account panel -->
+            <template v-slot:dropdown>
+              <div v-if="isLogged" class="flex flex-wrap account-panel p-4 w-60">
+                <h4>{{ $t('messages.welcome') }}</h4>
+                <p>{{ userName }}</p>
+                <el-divider />
+                <div class="panel-button pb-2">
+                  <router-link to="/myaccount">
+                    <el-button size="large" link>{{ $t('buttons.my-account') }}</el-button>
+                  </router-link>
+                </div>
+                <div class="panel-button">
+                  <el-button size="large" link @click="handleLogout">{{ $t('buttons.logout') }}</el-button>
+                </div>
+              </div>
+              <div v-if="!isLogged" class="p-4 w-60">
+                <router-link to="/login">
+                  <el-button class="w-full" color="#253343" size="large" round>{{ $t('buttons.login') }}</el-button>
+                </router-link>
+              </div>
+            </template>
+          </el-dropdown>
         </div>
 
       </div>
     </div>
+
+    <!-- Navbar vertical mobile -->
     <div class="nav-vertical">
       <el-menu v-if="showMenu" text-color="white" active-text-color="#00B561">
         <el-menu-item v-for="({ path, name, anchor }, index) in pages" :id="name" :key="index" :index="path" @click="toggleMenu">
-          <a :href="'#' + anchor">{{name}}</a>
+          <a :href="'#' + anchor">
+            <span>{{name}}</span>
+          </a>
         </el-menu-item>
       </el-menu>
     </div>
@@ -133,6 +185,12 @@ onBeforeMount(() => {
 <style scoped>
 .el-menu span {
   font-weight: 600;
+}
+.el-button.is-plain {
+  --el-button-hover-text-color: white !important;
+  --el-button-border-color: #253343 !important;
+  --el-button-hover-border-color: #253343 !important;
+  --el-button-hover-bg-color: #253343 !important;
 }
 .nav-horizontal .el-menu {
   background-color: transparent;
@@ -145,7 +203,6 @@ onBeforeMount(() => {
 .nav-horizontal .el-menu-item.is-active {
   border-bottom-width: 3px;
   --el-menu-hover-bg-color: transparent;
-
 }
 .nav-vertical .el-menu {
   position: absolute;
@@ -153,5 +210,8 @@ onBeforeMount(() => {
   width: 100%;
   border: 0;
   --el-menu-hover-bg-color: transparent;
+}
+.panel-button .el-button {
+  --el-button-hover-link-text-color: #00B561;
 }
 </style>
